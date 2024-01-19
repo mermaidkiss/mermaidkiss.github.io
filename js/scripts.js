3f5adef61d60c875a1ce15154ce93618fb89f490
mermaidkiss <thegame.stf@gmail.com>
2024-01-20 03:39:53 +0530

    update final release

--- js/scripts.js
+++ js/scripts.js
@@ -189,23 +189,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Ravi and Gayathry's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Feb 16, 2024 18:30'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Feb 18, 2024 12:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Malaysia',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact at +91 9791093062."
         }
     });
 
@@ -223,7 +223,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyzNOe_uEOMkbBLaDdWsg7P2wgbA8n_b8vL7Ynb9Jr1FY_7GZuaRG-1eltng9dLfpEs-w/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
